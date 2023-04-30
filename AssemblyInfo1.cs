bbc264d37798c5d24c1cf7bdf076308853ef579a
Farukon <132124585+Farukon-0@users.noreply.github.com>
2023-04-30 11:24:41 +1000

    Minor Update to AssemblyInfo1.cs

--- AssemblyInfo1.cs
+++ AssemblyInfo1.cs
@@ -2,7 +2,7 @@ using MelonLoader;
 using Movement_Speed_Cap_Plus;
 using System.Reflection;
 
-[assembly: MelonInfo(typeof(MoveSpeedCap), "Move Speed Cap", "1.0.0", "Farukon")]
+[assembly: MelonInfo(typeof(MoveSpeedCap), "Move Speed Cap +", "1.0.0", "Farukon")]
 [assembly: MelonGame("poncle", "VampireSurvivors")]
-[assembly: VerifyLoaderVersion(0, 6, 0, true)]
-[assembly: MelonAuthorColor(0, 255, 0, 255)]
+[assembly: VerifyLoaderVersion(0, 6, 1, true)]
+[assembly: MelonAuthorColor(255, 0, 255, 0)]
