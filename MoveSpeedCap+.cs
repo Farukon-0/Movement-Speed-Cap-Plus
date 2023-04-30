f5ae2d3f1003f9a9dc71eda72a898c20f9a8e595
Farukon <132124585+Farukon-0@users.noreply.github.com>
2023-04-30 11:56:03 +1000

    Update MoveSpeedCap+.cs

--- MoveSpeedCap+.cs
+++ MoveSpeedCap+.cs
@@ -20,7 +20,7 @@ namespace Movement_Speed_Cap_Plus
     public class ConfigData
     {
         public bool Enabled { get; set; } = true;
-        public ConfigSettingData MoveSpeedAdd { get; set; } = new ConfigSettingData("Increases or descreases the movement speed of all characters by a set amount. E.g. 0.11 adds 11% to your movespeed. (Opposite effect on negative.)", "Default = 0.0", 0.0f);
+        public ConfigSettingData AddMoveSpeed { get; set; } = new ConfigSettingData("Increases or descreases the base movement speed of all characters by a set amount. E.g. 0.11 adds 11% to your movespeed. (Opposite effect on negative.)", "Default = 0.0", 0.0f);
         public ConfigSettingData MinMoveSpeed { get; set; } = new ConfigSettingData("The minimum your movement speed can be. E.g. -0.55 means you'll always have more than -155% movement speed ingame.", "Default = -1.0", -1.0f);
         public ConfigSettingData MaxMoveSpeed { get; set; } = new ConfigSettingData("The maximum your movement speed can be. E.g. 1.58 you'll never have more than +58% movement speed ingame.", "Default = 3.0", 3.0f);
     }
@@ -45,11 +45,11 @@ namespace Movement_Speed_Cap_Plus
         static readonly string _ConfigPath = Path.Combine(_ConfigFolderPath, "MoveSpeedPlusConfig.json");
 
         static readonly string EnabledKey = "Enabled";
-        static readonly string MoveSpeedAddKey = "MoveSpeedAdd";
+        static readonly string AddMoveSpeedKey = "AddMoveSpeed";
         static readonly string MinMoveSpeedKey = "MinMoveSpeed";
         static readonly string MaxMoveSpeedKey = "MaxMoveSpeed";
         static bool _Enabled;
-        static float _MoveSpeedAdd;
+        static float _AddMoveSpeed;
         static float _MinMoveSpeed;
         static float _MaxMoveSpeed;
 
@@ -73,7 +73,7 @@ namespace Movement_Speed_Cap_Plus
             "Too much movement? Try Move Speed Cap+!",
             "With Move Speed Cap+, you can finally talk to Trousers again!",
             "Avatar Infernas *may* be overpowered with Move Speed Cap+",
-            "In MoveSpeedConfig.json, you can also set a minimum!\n (WARNING: If the minimum is changed, it HAS to be smaller than the max!)",
+            "In the MoveSpeedPlusConfig.json, you can also set a minimum!\n (WARNING: If the minimum is changed, it HAS to be smaller than the max!)",
             "If you so choose, you can set the cap to a *negative!\n (*The negative value shown in game is shown when below 1, this will slow you to a halt at 0.)",
             "The point where your controls are inverted is at a negative for the config, below -100% ingame!",
             "REJECT SPEED, EMBRACE AVERAGE!!! Coutesy of Move Speed Cap+.",
@@ -102,7 +102,7 @@ namespace Movement_Speed_Cap_Plus
             {
                 if (_Enabled)
                 {
-                    float _MoveSpeed = __instance._playerStats._MoveSpeed_k__BackingField + _MoveSpeedAdd;
+                    float _MoveSpeed = __instance._playerStats._MoveSpeed_k__BackingField + _AddMoveSpeed;
                     if (_MoveSpeed > _MaxMoveSpeed) _MoveSpeed = _MaxMoveSpeed;
                     if (_MoveSpeed < _MinMoveSpeed) _MoveSpeed = _MinMoveSpeed;
                     __instance._playerStats._MoveSpeed_k__BackingField = _MoveSpeed;
@@ -158,7 +158,7 @@ namespace Movement_Speed_Cap_Plus
         {
             JObject configJson = JObject.Parse(File.ReadAllText(_ConfigPath) ?? "{}");
             _Enabled = configJson.Value<bool>(EnabledKey);
-            _MoveSpeedAdd = configJson.Value<float>(MoveSpeedAddKey);
+            _AddMoveSpeed = configJson.Value<float>(AddMoveSpeedKey);
             _MinMoveSpeed = configJson.Value<float>(MinMoveSpeedKey);
             _MaxMoveSpeed = configJson.Value<float>(MaxMoveSpeedKey);
         }
