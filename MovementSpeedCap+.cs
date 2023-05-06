7fe3e80d957b9f41ecf03f26a991210151feab01
Farukon <132124585+Farukon-0@users.noreply.github.com>
2023-05-06 10:41:56 +1000

    Updated MovementSpeedCap+.cs
    Now includes compatibility for LeCloutPanda's ModMenu as well as a toggle for min and max values.

--- MovementSpeedCap+.cs
+++ MovementSpeedCap+.cs
@@ -33,10 +33,14 @@ namespace Movement_Speed_Cap_Plus
         static readonly string _ConfigPath = Path.Combine(_ConfigFolderPath, "MoveSpeedPlusConfig.json");
 
         static readonly string EnabledKey = "Enabled";
+        static readonly string MinEnabledKey = "MinEnabled";
         static readonly string MinMoveSpeedKey = "MinMoveSpeed";
+        static readonly string MaxEnabledKey = "MaxEnabled";
         static readonly string MaxMoveSpeedKey = "MaxMoveSpeed";
         static bool _Enabled;
+        static bool _MinEnabled;
         static float _MinMoveSpeed;
+        static bool _MaxEnabled;
         static float _MaxMoveSpeed;
 
         static void UpdateToggleDebug(bool value) => UpdateEnabled(value);
@@ -67,6 +71,7 @@ namespace Movement_Speed_Cap_Plus
             "A Move Speed Capper shouldn't have this number of random load messages. \n[LMAO]But why not?",
             "A mod to add basic configurable stuff to Move Speed shouldn't have taken me a week to make.",
             "Thanks to Takacomic for making this mod work!",
+            "Now you can disable the Min and Max in the Config!",
             "!dedaol neeb sah doM +paC deepS evoM ehT"
         };
 
@@ -80,6 +85,21 @@ namespace Movement_Speed_Cap_Plus
             }
         }
 
+        DateTime lastModified;
+
+        public override void OnLateUpdate()
+        {
+            base.OnLateUpdate();
+
+            DateTime lastWriteTime = File.GetLastWriteTime(_ConfigPath);
+
+            if (lastModified != lastWriteTime)
+            {
+                lastModified = lastWriteTime;
+                LoadConfig();
+            }
+        }
+
         [HarmonyPatch(typeof(CharacterController), "OnUpdate")]
         public class ModifierStatsMovespeed_Patch
         {
@@ -89,8 +109,14 @@ namespace Movement_Speed_Cap_Plus
                 if (_Enabled)
                 {
                     float _MoveSpeed = __instance._playerStats._MoveSpeed_k__BackingField;
-                    if (_MoveSpeed > _MaxMoveSpeed) _MoveSpeed = _MaxMoveSpeed;
-                    if (_MoveSpeed < _MinMoveSpeed) _MoveSpeed = _MinMoveSpeed;
+                    if (_MaxEnabled)
+                    {
+                        if (_MoveSpeed < _MinMoveSpeed) _MoveSpeed = _MinMoveSpeed;
+                    }
+                    if (_MinEnabled)
+                    {
+                        if (_MoveSpeed < _MinMoveSpeed) _MoveSpeed = _MinMoveSpeed;
+                    }
                     __instance._playerStats._MoveSpeed_k__BackingField = _MoveSpeed;
                 }
             }
@@ -144,7 +170,9 @@ namespace Movement_Speed_Cap_Plus
         {
             JObject configJson = JObject.Parse(File.ReadAllText(_ConfigPath) ?? "{}");
             _Enabled = configJson.Value<bool>(EnabledKey);
+            _MinEnabled = configJson.Value<bool>(MinEnabledKey);
             _MinMoveSpeed = configJson.Value<float>(MinMoveSpeedKey);
+            _MaxEnabled = configJson.Value<bool>(MaxEnabledKey);
             _MaxMoveSpeed = configJson.Value<float>(MaxMoveSpeedKey);
         }
     }
